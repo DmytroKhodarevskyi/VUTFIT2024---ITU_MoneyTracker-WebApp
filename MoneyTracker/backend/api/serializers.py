8ca76c6d6d7f6ab89c6cc9fa2e3b93c8e8141698
Dmytro Khodarevskyi <xkhoda01@vutbr.cz>
2024-08-18 14:51:18 +0200

    register page and login styling

--- MoneyTracker/backend/api/serializers.py
+++ MoneyTracker/backend/api/serializers.py
@@ -5,11 +5,19 @@ from .models import Note
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "password"]
-        extra_kwargs = {"password": {"write_only": True, "required": True}}
+        fields = ["id", "username", "password", "email"]
+        extra_kwargs = {
+            "password": {"write_only": True, "required": True},
+            "email": {"required": True}
+        }
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        # user = User.objects.create_user(**validated_data)
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
         return user
     
 class NoteSerializer(serializers.ModelSerializer):
--- MoneyTracker/frontend/src/components/Form.jsx
+++ MoneyTracker/frontend/src/components/Form.jsx
@@ -1,60 +1,158 @@
 import { useState } from "react";
 import api from "../api"
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 import "../styles/Form.css"
 
 function Form({route, method}){
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
+    const [repeatPassword, setRepeatPassword] = useState("")
+    const [email, setEmail] = useState("")
     const [loading, setLoading] = useState("")
     const navigate = useNavigate()
-    const name = method === "login" ? "Login" : "Register"
+    const name = method === "login" ? "Who Is it?" : "Introduce Yourself."
+    const button = method === "login" ? "Login" : "Register"
+
 
     const handleSubmit = async (e) => {
         setLoading(true);
         e.preventDefault()
 
+        if (method === "register" && password !== repeatPassword) {
+            alert("Passwords do not match")
+            setLoading(false)
+            return;
+        }
+
         try {
-            const res = await api.post(route, {username, password})
+            const payload = { username, password };
+            if (method === "register") {
+                payload.email = email;
+            }
+
+            const res = await api.post(route, payload);
             if (method === "login") {
                 localStorage.setItem(ACCESS_TOKEN, res.data.access);
                 localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
-                navigate("/")
+                navigate("/");
             } else {
-                navigate("/login")
+                navigate("/login");
             }
         } catch (error) {
-            alert(error)
+            alert(error);
         } finally {
-            setLoading(false)
+            setLoading(false);
         }
+
+        // try {
+        //     const res = await api.post(route, {username, password})
+        //     if (method === "login") {
+        //         localStorage.setItem(ACCESS_TOKEN, res.data.access);
+        //         localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+        //         navigate("/")
+        //     } else {
+        //         navigate("/login")
+        //     }
+        // } catch (error) {
+        //     alert(error)
+        // } finally {
+        //     setLoading(false)
+        // }
     }
 
-    return  (
-        <form onSubmit={handleSubmit} className="form-container" >
-            <h1> {name} </h1>
-            <input
-                className="form-input"
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                placeholder="Username"
-            />
+    // return  (
+    //     <form onSubmit={handleSubmit} className="form-container" >
+    //         <h1> {name} </h1>
 
-            <input
-                className="form-input"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Password"
-            />
+    //         <input
+    //             className="form-input"
+    //             type="text"
+    //             value={username}
+    //             onChange={(e) => setUsername(e.target.value)}
+    //             placeholder="Username"
+    //         />
 
-            <button className="form-button" type="submit">
-                {name}
-            </button>
+    //         <input
+    //             className="form-input"
+    //             type="password"
+    //             value={password}
+    //             onChange={(e) => setPassword(e.target.value)}
+    //             placeholder="Password"
+    //         />
+
+    //         <button className="form-button" type="submit">
+    //             {name}
+    //         </button>
+    //     </form>
+    // )
+
+    return (
+        <form onSubmit={handleSubmit} className="form-container">
+            <h1 className="form-headertext">{name}</h1>
+
+            <div className="inputbtn-container">
+                <div className="input-container">
+
+                <input
+                    className="form-input"
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    placeholder="Username"
+                />
+
+                {method === "register" && (
+                    <input
+                        className="form-input"
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Email"
+                    />
+                )}
+
+                <input
+                    className="form-input"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Password"
+                />
+
+                {method === "register" && (
+                    <input
+                        className="form-input"
+                        type="password"
+                        value={repeatPassword}
+                        onChange={(e) => setRepeatPassword(e.target.value)}
+                        placeholder="Repeat Password"
+                    />
+                )}
+                </div>
+
+                <button className="form-button" type="submit" disabled={loading}>
+                    <p className="form-button-text"> {button} </p> 
+                </button>
+
+            </div>
+                {method === "login" && (
+                    <div className="form-registerbtn-container">
+                        <p className="form-notregistered">Not registered?</p>
+                        
+                        <Link to="/register">
+                            <p className="form-create-account">Create an Account!</p>
+                        </Link>
+                    </div>
+                )}
+
+                {method === "register" && (
+                    <Link to="/login">
+                        <a className="form-goback">Go Back</a>
+                    </Link>
+                )}
         </form>
-    )
+    );
 }
 
 export default Form
