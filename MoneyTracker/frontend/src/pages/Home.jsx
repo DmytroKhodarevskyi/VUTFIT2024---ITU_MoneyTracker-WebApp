341cff6b5cdb2dcab2cd2a2fc84bc03b575523bd
Dmytro Khodarevskyi <xkhoda01@vutbr.cz>
2024-08-19 21:50:16 +0200

    cards add (cards width container needs to be fixed by main container)

--- MoneyTracker/frontend/src/pages/Home.jsx
+++ MoneyTracker/frontend/src/pages/Home.jsx
@@ -4,6 +4,9 @@ import "../styles/Home.css"
 import MainContainer from "../components/MainContainer"
 import TopPart from "../components/TopPart"
 import SummaryCard from "../components/SummaryCard"
+import WalletIcon from "../assets/wallet-icon.svg"
+import BagIcon from "../assets/BagIcon.svg"
+import CardIcon from "../assets/CardIcon.svg"
 
 function Home() {
 
@@ -20,7 +23,23 @@ function Home() {
                         amount={currency + "1.000.000,01"}
                         trends={"+52% LastYear"}
                         style_trends={{color: '#00BCD4'}}
-                        img_src={"somepath"}
+                        img_src={WalletIcon}
+                    />
+                    <SummaryCard 
+                        title={"Total Income"}
+                        date={"2 September - 1 July 2024"}
+                        amount={currency + "520.000,01"}
+                        trends={"+12% Last Month"}
+                        style_trends={{color: '#4CAF50'}}
+                        img_src={CardIcon}
+                    />
+                    <SummaryCard 
+                        title={"Total Spending"}
+                        date={"2 September - 1 July 2024"}
+                        amount={currency + "228.000,00"}
+                        trends={"+15% LastYear"}
+                        style_trends={{color: '#F44336'}}
+                        img_src={BagIcon}
                     />
                 </div>
             </MainContainer>
