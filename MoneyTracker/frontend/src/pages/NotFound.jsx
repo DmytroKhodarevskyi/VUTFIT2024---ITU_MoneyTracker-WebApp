a4af7b972d73c763c2be2208ffedbdbf3c943ade
DenyysChernenko <xchern08@stud.fit.vutbr.cz>
2024-08-20 22:29:38 +0200

    NotFound make prettier

--- MoneyTracker/frontend/src/pages/NotFound.jsx
+++ MoneyTracker/frontend/src/pages/NotFound.jsx
@@ -1,5 +1,9 @@
+import "../styles/NotFound.css"
+
 function NotFound() {
-    return <div>The page you're looking for does not exist!</div>
+    return <div class="text-container">
+        <p> The page you're looking for does not exist😥</p>
+        </div>
 }
 
 export default NotFound
