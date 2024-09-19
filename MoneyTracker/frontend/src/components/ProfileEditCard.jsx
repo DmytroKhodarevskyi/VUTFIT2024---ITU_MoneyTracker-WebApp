0364b25c209c26da3b61050329ba8975b345ad3e
DenyysChernenko <xchern08@stud.fit.vutbr.cz>
2024-09-19 13:56:26 +0200

    added profile edit component

---
+++ MoneyTracker/frontend/src/components/ProfileEditCard.jsx
@@ -0,0 +1,167 @@
+import React from 'react';
+import "../styles/ProfileEdit.css";
+
+function ProfileEditCard({
+    profileImg, 
+    firstname,
+    lastname,
+    fullname, 
+    jobTitle, 
+    email, 
+    phone, 
+    country,
+    city,
+    gender,
+    genderChoices,
+    fileInputRef,
+    handleInputChange,
+    handleSave,
+    handleFileChange,
+    handleFileSelect,
+    handleUploadPhoto,
+    handleDeletePhoto,
+    photoPreview,
+    deletePhoto
+}) {
+
+    const baseUrl = import.meta.env.VITE_API_URL;
+    let displayImageUrl;
+
+    if (deletePhoto) {
+        displayImageUrl = `${baseUrl}/media/profile_images/default.png`
+    } else if (photoPreview) {
+        displayImageUrl = photoPreview;
+    } else {
+        displayImageUrl = `${baseUrl}${profileImg}`;
+    }
+
+    return (
+        <div className="profile-wrapper">
+            <div className="profile-details-card">
+                <div className="profile-header-section">
+                    <div className="profile-picture-section">
+                        <img 
+                            draggable="false"
+                            src={displayImageUrl} 
+                            alt={`${fullname}'s profile`} 
+                            className="profile-picture" 
+                        />
+                    </div>
+                <div className="header-info-section">
+                    <div className="photo-buttons">
+                        <input
+                            type="file"
+                            accept="image/*"
+                            ref={fileInputRef}
+                            style={{ display: 'none' }} 
+                            onChange={handleFileChange}
+                            onInput={handleFileSelect} 
+                        />
+                        <button className="upload-photo-button" onClick={handleUploadPhoto}>Upload a new photo</button>
+                        <button className="delete-photo-button" onClick={handleDeletePhoto}>Delete Photo</button>
+                    </div>
+                    <div className="details-section">
+                        <div className='info-item'>
+                        <label className="label-first-name">Name</label>
+                        <input 
+                            type="text" 
+                            className="input-name" 
+                            value={firstname} 
+                            onChange={e => handleInputChange('firstname', e.target.value)} 
+                            placeholder="Name"
+                        />
+                        </div>
+                        <div className="info-item">
+                            <label className="label-last-name">Surname</label>
+                            <input 
+                                type="text" 
+                                className="input-name" 
+                                value={lastname} 
+                                onChange={e => handleInputChange('lastname', e.target.value)} 
+                                placeholder="Surname"
+                            />
+                        </div>
+                        <div className='info-item'>
+                        <label className="label-contact">Phone</label>
+                        <input 
+                            type="tel" 
+                            className="input-box" 
+                            value={phone} 
+                            onChange={e => handleInputChange('phone', e.target.value)} 
+                            placeholder="Phone"
+                        />
+                        </div>
+                    </div>
+                </div>
+            </div>
+                    
+                <div className="personal-info-section">
+                    <div className="info-item">
+                        <label className="label-contact">Email</label>
+                            <input 
+                                type="email" 
+                                className="input-box" 
+                                value={email} 
+                                onChange={e => handleInputChange('email', e.target.value)} 
+                                placeholder="Email"
+                            />
+                    </div>
+                    <div className="info-item">
+                        <label className='label-job-title'> Job </label>
+                            <input 
+                                type="text" 
+                                className="input-job-title" 
+                                value={jobTitle} 
+                                onChange={e => handleInputChange('jobTitle', e.target.value)} 
+                                placeholder="Job Title"
+                            />
+                    </div>
+                    <div className="info-item">
+                        <label className="label-info">Gender</label>
+                        <select 
+                            className="input-box" 
+                            value={gender} 
+                            onChange={e => handleInputChange('gender', e.target.value)}>
+                            {genderChoices.map(choice => (
+                                <option key={choice.value} value={choice.value}>
+                                    {choice.label}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className='adress-info'>
+                        <div className="info-item">
+                            <label className="label-info">Country</label>
+                            <input 
+                                type="text" 
+                                className="input-box" 
+                                value={country} 
+                                onChange={e => handleInputChange('country', e.target.value)} 
+                                placeholder="Country"
+                            />
+                        </div>
+                        <div className="info-item">
+                            <label className="label-info">City</label>
+                            <input 
+                                type="text" 
+                                className="input-box" 
+                                value={city} 
+                                onChange={e => handleInputChange('city', e.target.value)} 
+                                placeholder="City"
+                            />
+                         </div>
+                    </div>    
+                </div>
+            </div>
+
+            <div className="sidebar-actions">
+                <div className="action-buttons">
+                    <button className="button-save-profile" onClick={handleSave}>Save</button>
+                    <button className="button-discard-profile">Discard</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+    
+export default ProfileEditCard;
