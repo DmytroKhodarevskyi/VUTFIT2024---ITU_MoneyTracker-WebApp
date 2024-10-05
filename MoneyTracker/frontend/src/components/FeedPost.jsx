51fc14948dd9fccd47a4101e653675102ddae0f5
Dmytro Khodarevskyi <xkhoda01@vutbr.cz>
2024-10-05 22:42:16 +0200

    first try to add posts scrolling in feed (will be changed later)

--- MoneyTracker/frontend/src/components/FeedPost.jsx
+++ MoneyTracker/frontend/src/components/FeedPost.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { useState, useEffect } from "react"
-import "../styles/MyFeedCard.css";
+import "../styles/FeedPost.css";
 import star_picture from "./star.svg";
 import comment_picture from "./comment.svg";
 import api from '../api';
-function FeedPost ({
-    publication
-}) 
-{
 
+function FeedPost ({ publication, IsCenter, IsLeft, IsRight, shouldDisplay }) {
 
     const {author, title, content_text, content_media, tags, stars } = publication; 
 
@@ -19,9 +16,23 @@ function FeedPost ({
     
     const [isLoaded, setIsLoaded] = useState(false);
 
+    const [CardStyle, setCardStyle] = useState({});
 
     useEffect(() => {
+      console.log(`Post ID: ${publication.id}, shouldDisplay: ${shouldDisplay}`);
+  }, [shouldDisplay, publication.id]);
 
+    if (!shouldDisplay) {
+        return (
+          <div className="FeedPost-card-container" 
+              style={CardStyle}
+          >
+          </div>
+        );
+      }
+
+
+    useEffect(() => {
         const fetchAuthor = async () => {
             try {
                 const response = await api.get(`/api/user/profile/${author.id}/`);
@@ -40,6 +51,55 @@ function FeedPost ({
     }, []);
 
 
+  useEffect(() => {
+    if (IsCenter) {
+        setCardStyle({
+            backgroundColor: '#FFFFFF',
+            zIndex: 10,          
+            'box-shadow': 'rgba(20, 33, 61, 0.1) 0px 4px 10px',
+        });
+
+    } else if (IsLeft) {
+        setCardStyle({
+            "margin-right": '-200px',
+            backgroundColor: '#FCFCFC',
+            zIndex: 5,
+            transform: 'scale(0.96)',
+            transformOrigin: 'bottom left',
+            width: '60%',
+            left: '0%',
+            filter: 'blur(1px)',
+
+            overflow: 'hidden',
+        });
+
+
+    } else if (IsRight) {
+          setCardStyle({
+            "margin-left": '-200px',
+            backgroundColor: '#FCFCFC',
+            zIndex: 5,
+            transformOrigin: 'right',
+            width: '60%',
+            transform: 'scale(0.96)',
+            transformOrigin: 'bottom right',
+            filter: 'blur(1px)',
+
+            overflow: 'hidden',
+
+        });
+
+    } else {
+        setCardStyle({
+            backgroundColor: '#FCFCFC',
+            zIndex: 1,                        
+            opacity: 0.5,                     
+            transform: 'scale(0.8)',          
+        });
+    }
+}, [IsCenter, IsLeft, IsRight]);
+
+
     const baseUrl = import.meta.env.VITE_API_URL;
     // const fullImageUrl = profileImg ? `${baseUrl}${profileImg}` : `${baseUrl}media/profile_images/default.png`;
 
@@ -48,50 +108,105 @@ function FeedPost ({
     : `${baseUrl}${content_media}`;
 
       return (
-        <div className="post-container">
-          <div className="post-card">
-            <div className="post-header">
-              <div className="post-profile-image">
-                <img src={profileImg} alt={`profile img`} className="profile-image" draggable="false" />
-              </div>
-              <div className="post-author">
-                <p className="post-fullname">{Name} {Surname}</p>
-              </div>
-            </div>    
-            <div className="post-content">
-                <div className="post-name">{title || "Untitled Post"}</div>
-                    <p>{content_text || "No content available."}</p>
-                    {content_media && (
-                         <img src={fullContentMediaUrl} alt="Publication media" className="post-media" />
-                    )}
-                    <div className='tag-container'>
-                      <span className='post-tags'>Tags: </span>
-                      {tags  ? (
-                        tags.split(' ').map((tag, index) => (
-                          <span key={index} className="post-hashtag">#{tag} </span>
-                        ))
-                      ) : (
-                        <span className='post-hashtag'>None</span>
-                      )}
+      // <div className="FeedPost-card-main-container"
+      //     style={ContainerStyle}>
+
+        <div className="FeedPost-card-container" 
+              style={CardStyle}
+        >
+              <div className="FeedPost-card-top">
+                <div className="FeedPost-card-header">
+                    <div className="FeedPost-card-profile-image">
+                        <img src={profileImg} alt={`profile img`} className="FeedPost-profile-image" draggable="false" />
                     </div>
-            </div>
-
-            <div className="post-footer">
-              <div className="post-actions">
-                <button className="like-button">
-                      I Like this! <span className="like-count">{stars}</span> 
-                      <img src={star_picture} alt="Star" className="star-icon" />
-                </button>
-
-                <button className="comment-button">
-                      Comment <span className="comment-count">3</span> 
-                      <img src={comment_picture} alt="Comment" className="comment-icon" />
-                </button>
+                    <div className="FeedPost-card-author">
+                        <p className="FeedPost-card-fullname">{Name} {Surname}</p>
+                    </div>
+                </div>    
+                <div className="FeedPost-card-content">
+                    <div className="FeedPost-card-name">{title || "Untitled Post"}</div>
+                        <p className='FeedPost-content-text'>{content_text || "No content available."}</p>
+                        {content_media && (
+                          <img src={fullContentMediaUrl} alt="Publication media" className="FeedPost-card-media" />
+                        )}
+                </div>
               </div>
 
-            </div>
-          </div>
+
+                <div className="FeedPost-card-footer">
+                    <div className='FeedPost-tag-container'>
+                        <span className='FeedPost-card-tags'>Tags: </span>
+                        {tags  ? (
+                            tags.split(' ').map((tag, index) => (
+                            <span key={index} className="FeedPost-card-hashtag">#{tag} </span>
+                            ))
+                        ) : (
+                            <span className='FeedPost-card-hashtag'>None</span>
+                        )}
+                    </div>
+
+                    <div className="FeedPost-card-actions">
+                        <button className="like-button">
+                            I Like this! <span className="like-count">{stars}</span> 
+                            <img src={star_picture} alt="Star" className="star-icon" />
+                        </button>
+
+                        <button className="comment-button">
+                            Comment <span className="comment-count">3</span> 
+                            <img src={comment_picture} alt="Comment" className="comment-icon" />
+                        </button>
+                    </div>
+
+                </div>
         </div>
+
+      // </div>
+
+
+        // <div className="post-container">
+        //   <div className="post-card">
+        //     <div className="post-header">
+        //       <div className="post-profile-image">
+        //         <img src={profileImg} alt={`profile img`} className="profile-image" draggable="false" />
+        //       </div>
+        //       <div className="post-author">
+        //         <p className="post-fullname">{Name} {Surname}</p>
+        //       </div>
+        //     </div>    
+        //     <div className="post-content">
+        //         <div className="post-name">{title || "Untitled Post"}</div>
+        //             <p>{content_text || "No content available."}</p>
+        //             {content_media && (
+        //                  <img src={fullContentMediaUrl} alt="Publication media" className="post-media" />
+        //             )}
+        //             <div className='tag-container'>
+        //               <span className='post-tags'>Tags: </span>
+        //               {tags  ? (
+        //                 tags.split(' ').map((tag, index) => (
+        //                   <span key={index} className="post-hashtag">#{tag} </span>
+        //                 ))
+        //               ) : (
+        //                 <span className='post-hashtag'>None</span>
+        //               )}
+        //             </div>
+        //     </div>
+
+        //     <div className="post-footer">
+        //       <div className="post-actions">
+        //         <button className="like-button">
+        //               I Like this! <span className="like-count">{stars}</span> 
+        //               <img src={star_picture} alt="Star" className="star-icon" />
+        //         </button>
+
+        //         <button className="comment-button">
+        //               Comment <span className="comment-count">3</span> 
+        //               <img src={comment_picture} alt="Comment" className="comment-icon" />
+        //         </button>
+        //       </div>
+
+        //     </div>
+        //   </div>
+        // </div>
       );
     }
     
--- MoneyTracker/frontend/src/pages/Feed.jsx
+++ MoneyTracker/frontend/src/pages/Feed.jsx
@@ -4,6 +4,7 @@ import MainContainer from "../components/MainContainer"
 import TopPart from "../components/TopPart"
 import { useNavigate } from 'react-router-dom'
 import FeedPost from "../components/FeedPost"
+import "../styles/FeedPost.css"
 
 
 function Feed () {
@@ -20,18 +21,28 @@ function Feed () {
 
     const [publications, setPublications] = useState([]); 
 
+    const [currentIndex, setCurrentIndex] = useState(0); // Track the current index
+
+    const [CenterIdx, setCenterIdx] = useState(1); // Track the center index
+
     // const navigate = useNavigate();
+
 
     useEffect(() => {
         const fetchNickname = async () => {
           try {
             
             const response = await api.get("/api/publications/feed/");
+            setPublications(response.data);
             setDisplayedPosts(response.data.slice(0, 3));
+
+            // setCenterIdx(Math.floor(displayedPosts.length / 2));
+            // console.log("CenterIdx: ", CenterIdx);
 
             const profileResponse = await api.get("/api/user/profile/");
 
-            console.log("Publications: ", response.data);
+            // console.log("Publications: ", response.data);
+            console.log(publications.length);
             // setNickname(response.data.username);
             // setName(response.data.first_name);
             setProfilePhoto(profileResponse.data.profileImg);
@@ -45,6 +56,29 @@ function Feed () {
         fetchNickname();
       }, []);
 
+      const handleNext = () => {
+        if (currentIndex + 3 < publications.length) {
+            setCurrentIndex(currentIndex + 1);
+            setDisplayedPosts(publications.slice(currentIndex + 1, currentIndex + 4));
+            setCenterIdx(1);
+        } else if (currentIndex + 3 === publications.length) {
+            setCurrentIndex(currentIndex + 1);
+            setDisplayedPosts(publications.slice(currentIndex + 1));
+            setCenterIdx(2);
+        }
+    };
+
+    const handlePrevious = () => {
+        if (currentIndex > 0) {
+            setCurrentIndex(currentIndex - 1);
+            setDisplayedPosts(publications.slice(currentIndex - 1, currentIndex + 2));
+            setCenterIdx(1);
+        } else if (currentIndex === 0) {
+            setDisplayedPosts(publications.slice(0, 2));
+            setCenterIdx(0);
+        }
+    };
+
     if (!isLoaded) {
         return (
             <div className="loading-container">
@@ -53,19 +87,60 @@ function Feed () {
         )
       }
 
-      return (
-        <MainContainer>
-        <TopPart nickname={profileData?.firstname}
-        selectedItem={"feed"} profilePhoto={profilePhoto}
-        titletext={"Let’s see what’s going on!"} subtitletext={"Scroll through posts, gain popularity by commenting"}/>
-        
-        {displayedPosts.map((post) => (
-            <FeedPost
-                key={post.id}
-                publication={post}
-            />
-        ))}
+    // const CenterIdx = Math.floor(displayedPosts.length / 2);
 
+      return (
+        // <MainContainer>
+        // <TopPart nickname={profileData?.firstname}
+        // selectedItem={"feed"} profilePhoto={profilePhoto}
+        // titletext={"Let’s see what’s going on!"} subtitletext={"Scroll through posts, gain popularity by commenting"}/>
+        
+        // {displayedPosts.map((post) => (
+        //     <FeedPost
+        //         key={post.id}
+        //         publication={post}
+        //     />
+        // ))}
+
+        // </MainContainer>
+
+        <MainContainer>
+            <TopPart
+                nickname={profileData?.firstname}
+                selectedItem={"feed"}
+                profilePhoto={profilePhoto}
+                titletext={"Let’s see what’s going on!"}
+                subtitletext={"Scroll through posts, gain popularity by commenting"}
+            />
+
+            <div className="feed-wrapper">
+              <button 
+              onClick={handlePrevious} 
+              disabled={currentIndex === -1}
+              >
+                Previous
+              </button>
+              {console.log("centerIdx: ", CenterIdx)}
+                {displayedPosts.map((post, index) => (
+
+
+                    <FeedPost
+                        key={post.id}
+                        publication={post}
+                        IsCenter={index === CenterIdx}                 // Check if the post is center
+                        IsLeft={index === CenterIdx - 1}               // Check if the post is left of center
+                        IsRight={index === CenterIdx + 1} 
+                        shouldDisplay={currentIndex >= 0 && currentIndex < publications.length} // Pass a prop to indicate if the post should be displayed             // Check if the post is right of center
+                        // className={index === Math.floor(displayedPosts.length / 2) ? "feed-post center" : "feed-post side"}
+                    />
+                ))}
+                <button 
+                onClick={handleNext}
+                disabled={currentIndex + 3 > publications.length}
+                >
+                  Next
+                </button>
+            </div>
         </MainContainer>
     );
 }
