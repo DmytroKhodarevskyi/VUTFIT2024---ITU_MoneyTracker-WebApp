ddbc1d7f9aa6a1a19d1cdec02860a942639c80de
StudentUAR <rostyslavkachan@gmail.com>
2024-11-18 16:26:50 +0100

    Add admin changes

--- MoneyTracker/backend/api/custom_admin/serializers.py
+++ MoneyTracker/backend/api/custom_admin/serializers.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 from api.transaction.models import Transaction
 from api.category.models import Category
 from api.publication.models import Publication
+from api.group.models import Group
+
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,9 +14,14 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'first_name', 'last_name']  # Add other fields as needed
 
 class TransactionSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(
+        format="%d-%m-%Y %H:%M:%S",  # Формат для виводу
+        input_formats=["%d-%m-%Y %H:%M:%S", "%Y-%m-%dT%H:%M:%S.%fZ"]  # Формати для вводу
+    )
+
     class Meta:
         model = Transaction
-        fields = '__all__'  
+        fields = '__all__'
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -23,4 +31,9 @@ class CategorySerializer(serializers.ModelSerializer):
 class PublicationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Publication
+        fields = '__all__' 
+        
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
         fields = '__all__' 
