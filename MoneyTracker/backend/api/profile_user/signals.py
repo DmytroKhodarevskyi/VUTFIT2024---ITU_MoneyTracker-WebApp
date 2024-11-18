ce06a21a7c3bd5ce4095fac721941a08103d78d3
DenyysChernenko <xchern08@stud.fit.vutbr.cz>
2024-11-18 21:42:26 +0100

    small fixes in admin creation logic

--- MoneyTracker/backend/api/profile_user/signals.py
+++ MoneyTracker/backend/api/profile_user/signals.py
@@ -3,14 +3,20 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Profile
+from django.db import IntegrityError
 
 def generate_unique_phone():
     return f'{random.randint(1000000000, 9999999999)}'
 
+def get_unique_phone():
+    while True:
+        phone_number = generate_unique_phone()
+        if not Profile.objects.filter(phone=phone_number).exists():
+            return phone_number
+
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-
         profile = Profile.objects.create(user=instance)
 
         instance.first_name = "Admin Name"
@@ -22,7 +28,7 @@ def create_user_profile(sender, instance, created, **kwargs):
             profile.country = 'Admin Country'
             profile.city = 'Admin City'
             profile.gender = 'N'  
-            profile.phone = generate_unique_phone() 
+            profile.phone = get_unique_phone() 
             profile.job = 'Administrator'
             profile.profile_image = 'profile_images/default_admin.png'
             profile.stars_count = 10
