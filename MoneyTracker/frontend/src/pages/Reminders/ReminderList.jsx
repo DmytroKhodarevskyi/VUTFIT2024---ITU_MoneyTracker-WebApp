1cb952bb3de7af031cc5b30baeef2cb495644e1f
Dmytro Khodarevskyi <xkhoda01@vutbr.cz>
2024-11-20 18:06:29 +0100

    reminder css begin

--- MoneyTracker/frontend/src/pages/Reminders/ReminderList.jsx
+++ MoneyTracker/frontend/src/pages/Reminders/ReminderList.jsx
@@ -26,6 +26,8 @@ function ReminderList() {
         setNickname(response.data.first_name);
         setProfilePhoto(response.data.profileImg);
 
+        const remindersResponse = await api.get("/api/reminders/reminders/");
+
         setRemindersList([
           {
             id: 1,
@@ -78,6 +80,23 @@ function ReminderList() {
                 <tbody>
                   {remindersList.map((reminder) => (
                     <tr key={reminder.id}>
+                      {/* <td>
+                        <label
+                          style={{ display: "flex", alignItems: "center" }}
+                        >
+                          <input
+                            type="checkbox"
+                            name="category"
+                            value={category.name}
+                            checked={selectedCategories.includes(category.id)}
+                            onChange={() => handleCheckboxChange(category)}
+                          />
+                          <span className="CategoriesAndStatistics-custom-checkbox"></span>
+                          <p className="CategoriesAndStatistics-categories-names">
+                            {category.name}
+                          </p>
+                        </label>
+                      </td> */}
                       <td>{reminder.title}</td>
                       <td>{formatDate(reminder.deadline)}</td>
                       <td>{reminder.amount}</td>
