74a88935e0129d494800665a187a0c61ef705bc1
Dmytro Khodarevskyi <xkhoda01@vutbr.cz>
2024-11-20 17:50:22 +0100

    reminders front start

---
+++ MoneyTracker/frontend/src/pages/Reminders/ReminderList.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import api from "../../api";
+import MainContainer from "../../components/MainContainer/MainContainer";
+import TopPart from "../../components/TopPart/TopPart";
+import { useState, useEffect } from "react";
+import "./ReminderList.css";
+
+function ReminderList() {
+  const [nickname, setNickname] = useState("");
+  const [profilePhoto, setProfilePhoto] = useState(null);
+
+  const [remindersList, setRemindersList] = useState([]);
+
+  function formatDate(dateString) {
+    const date = new Date(dateString);
+    return `${date.toLocaleDateString("uk-UA")} ${date.toLocaleTimeString(
+      "uk-UA",
+      { hour: "2-digit", minute: "2-digit" }
+    )}`;
+  }
+
+  useEffect(() => {
+    async function fetchProfileData() {
+      try {
+        const response = await api.get("/api/user/profile/");
+        setNickname(response.data.first_name);
+        setProfilePhoto(response.data.profileImg);
+
+        setRemindersList([
+          {
+            id: 1,
+            title: "Buy milk",
+            deadline: Date.now(),
+            amount: 100,
+          },
+          {
+            id: 2,
+            title: "Buy bread",
+            deadline: Date.now(),
+            amount: 300,
+          },
+          {
+            id: 3,
+            title: "Buy cheese",
+            deadline: Date.now(),
+            amount: 200,
+          },
+        ]);
+      } catch (error) {
+        console.error("Failed to fetch profile data", error);
+      }
+    }
+    fetchProfileData();
+  }, []);
+
+  return (
+    <>
+      <MainContainer>
+        <TopPart
+          nickname={nickname}
+          selectedItem={"profile"}
+          profilePhoto={profilePhoto}
+        />
+        <div className="ReminderList-main-container">
+          <div className="ReminderList-left-part">
+            <h1>Reminders</h1>
+            <h2>Set up your notifiers</h2>
+
+            <div className="ReminderList-reminders-table">
+              <table>
+                <thead>
+                  <tr>
+                    <th>Title</th>
+                    <th>Deadline</th>
+                    <th>Amount</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {remindersList.map((reminder) => (
+                    <tr key={reminder.id}>
+                      <td>{reminder.title}</td>
+                      <td>{formatDate(reminder.deadline)}</td>
+                      <td>{reminder.amount}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div className="ReminderList-right-part">
+            <button>Create Reminder</button>
+          </div>
+        </div>
+      </MainContainer>
+    </>
+  );
+}
+
+export default ReminderList;
