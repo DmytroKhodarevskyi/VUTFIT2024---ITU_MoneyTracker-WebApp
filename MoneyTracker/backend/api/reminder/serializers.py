277469eef9c0b9ecab80d0b767fd6cbffbfa929d
DenyysChernenko <xchern08@stud.fit.vutbr.cz>
2024-11-20 18:00:30 +0100

    Added pop up for creating reminders

--- MoneyTracker/backend/api/reminder/serializers.py
+++ MoneyTracker/backend/api/reminder/serializers.py
@@ -5,3 +5,4 @@ class ReminderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reminder
         fields = ['id', 'user', 'created_at', 'deadline', 'title', 'amount']
+        read_only_fields  = ['id', 'user', 'created_at']
