c3080c7d271fd6c496db6879fc422e9da99a284b
Dmytro Khodarevskyi <xkhoda01@vutbr.cz>
2024-12-13 15:04:37 +0100

    removed secrets

--- MoneyTracker/backend/backend/settings.py
+++ MoneyTracker/backend/backend/settings.py
@@ -170,8 +170,18 @@ USE_I18N = True
 
 USE_TZ = True
 
+# AWS S3 Configuration
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')  
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY') 
+AWS_STORAGE_BUCKET_NAME = 'moneytrackerbucket'
+AWS_S3_REGION_NAME = 'eu-north-1' 
 
 
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_URL = 'static/'
 
